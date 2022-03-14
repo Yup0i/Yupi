14ac1c3180b17f08d22ba212aa206feb40bf55a1
Yup0i <88381879+Yup0i@users.noreply.github.com>
2022-03-14 15:15:56 +0200

    yupi
    1

---
+++ yupi.js
@@ -0,0 +1,7 @@
+echo "# Yupi" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Yup0i/Yupi.git
+git push -u origin main
